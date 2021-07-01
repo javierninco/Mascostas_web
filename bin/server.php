797465e61958702d847e5c071afef1e17a9fb51e
javier ninco <40526954+javierninco@users.noreply.github.com>
2021-07-01 00:56:54 -0500

    Update server.php

--- bin/server.php
+++ bin/server.php
@@ -13,7 +13,7 @@ $server = IoServer::factory(
                                 new Chat()
                         )
                 ),
-                80
+                8080
 );
 
 $server->run();
